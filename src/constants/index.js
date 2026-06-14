ad90af51aeefeb15d02759cb0b5e4b6de802a9fd
Ishant Shekhar Eeshu <ishantvats123@gmail.com>
2026-06-14 11:41:39 +0530

    V2.2

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,6 @@
     { id: 'skills', label: 'Skills' },
     { id: 'projects', label: 'Projects' },
     { id: 'experience', label: 'Experience' },
-    { id: 'education', label: 'Education' },
   ];
 
   export const SKILLS = [
